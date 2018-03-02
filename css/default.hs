a3351fe78835262fa19922a5b7fe55eb18a4434a
Magnus Møller <magnus108@me.com>
2018-03-02 22:28:01 +0000

    Modified some css

--- css/default.hs
+++ css/default.hs
@@ -2,6 +2,10 @@
 import           Clay
 
 
+primaryBoxShadow :: Css
+primaryBoxShadow = boxShadowWithSpread nil (px 2) (px 5) nil (rgba 0 0 0 0.3)
+
+
 darkPrimaryColor :: Color
 darkPrimaryColor = "#0097A7"
 
@@ -38,13 +42,23 @@ margin1 :: Size a -> Css
 margin1 x = margin x x x x
 
 
+padding1 :: Size a -> Css
+padding1 x = padding x x x x
+
+
 defaultStylesheet :: Css
 defaultStylesheet = do
     body ? do
         color primaryTextColor
         margin1 nil
     header ? do
+        padding1 (px 15)
         background darkPrimaryColor
+        primaryBoxShadow
+    main_ ? do
+        padding1 (px 15)
+    footer ? do
+        padding1 (px 15)
 
 
 main :: IO ()
