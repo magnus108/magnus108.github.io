3879a89f8bb2cf94ce0bf5a7ad71c31c70eaa8ed
Magnus Møller <magnus108@me.com>
2018-03-02 14:01:55 +0000

    Cleaned unused files

--- site.hs
+++ site.hs
@@ -7,15 +7,8 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match "contact.markdown" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
